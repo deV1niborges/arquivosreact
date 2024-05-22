a2087c13c98a4e8b36130d9364498f71d030c17a
deV1niborges <138510552+deV1niborges@users.noreply.github.com>
2024-05-22 13:46:52 -0300

    Initial commit

---
+++ fundamentos/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import FirstComponent from "./component/FirstComponent";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Fundamentos React</h1>
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
