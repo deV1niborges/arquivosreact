33c8a4f16c734e29af4676250c7efb2f415a8c8d
deV1niborges <138510552+deV1niborges@users.noreply.github.com>
2024-06-06 15:09:22 -0300

    update react

--- avancando_react/src/App.jsx
+++ avancando_react/src/App.jsx
@@ -3,4 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
+export function App() {
+  return (
+    <div className="App">
+      <h1>Avançando em React</h1>
+    </div>
+  )
+}
