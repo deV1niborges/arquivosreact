c016af8c461e0067bf450368f8cd7c91027695de
Vinicius Borges <138510552+deV1niborges@users.noreply.github.com>
2025-04-01 17:00:29 -0300

    update

--- avancando_react/src/App.jsx
+++ avancando_react/src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import City from "./assets/city.jpg";
 import { ManageData } from "./components/ManageData";
 import { ListRender } from "./components/ListRender";
 import { ConditionalRender } from "./components/ConditionalRender";
+import ShowUserName from "./components/ShowUserName";
 
 export function App() {
   return (
@@ -22,6 +20,7 @@ export function App() {
       <ManageData />
       <ListRender />
       <ConditionalRender />
+      <ShowUserName />
     </div>
   );
 }
---
+++ avancando_react/src/components/ShowUserName.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowUserName = () => {
+  return (
+    <div>ShowUserName</div>
+  )
+}
+
+export default ShowUserName
